a80b76f1de77bbd741883b5940a0b997262b125c
Phan Nhat Tan <phannhattan10062001@gmail.com>
2022-11-05 15:41:16 +0700

    commit

--- src/main/java/com/pnt/Employeemanagement.java
+++ src/main/java/com/pnt/Employeemanagement.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Employeemanagement {
 	public static void main(String[] args) {
-		SpringApplication.run(Employeemanagement.class, args);
+		SpringApplication.run(Employeemanagement.class, args);  
 	}
 }
